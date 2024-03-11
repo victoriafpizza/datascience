34ad3e63827816ff3c33b40171c662b119640950
victoriafpizza <rm550609@fiap.com.br>
2024-03-11 10:16:03 -0300

    lero

---
+++ ds/numpy/arrays.py
@@ -0,0 +1,18 @@
+# Como tranformar um lista em um array em Python
+
+import numpy as np
+
+# cria uma lista
+lista = [1, 2, 3, 4, 5]
+
+# transforma a lista em um array Numpy
+array = np.array(lista) #função de array
+
+print("Lista: ", lista)
+print("Array: ", array)
+
+# vantagens de ursar arrays 
+
+# Eficiência de processamento
+# Facilidade de uso
+# Integração com outras bibliotecas
